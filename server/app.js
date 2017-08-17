ea83d7da28ef807e6179fb67e413d0802d5145f9
Brendt Bly <brendt.bly@outlook.com>
2017-08-17 16:45:11 -0500

    set up files

---
+++ server/app.js
@@ -0,0 +1,15 @@
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var port = 5000;
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+//link pets
+
+
+app.listen(port, function(){
+    console.log('Running on port: ', port);
+});
