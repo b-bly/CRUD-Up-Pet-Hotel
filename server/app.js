39c07e9e869505902db243061c05664b5da3ca28
Nic Bade <chipchapmaniii@gmail.com>
2017-08-18 13:40:12 -0500

    connected the input fields (owner) to the database

--- server/app.js
+++ server/app.js
@@ -1,17 +1,18 @@
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var port = 5000;
 var pets = require('./routes/pets');
+var path = require('path');
 
 app.use(express.static('public'));
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 //link pets
 app.use('/pets', pets);
 
 
-app.listen(port, function(){
+app.listen(port, function() {
     console.log('Running on port: ', port);
 });
--- server/routes/pets.js
+++ server/routes/pets.js
@@ -4,4 +4,27 @@ var pool = require('../modules/pool.js');
 
 console.log('pets route is connected');
 
+router.post('/', function(req, res) {
+    var newOwner = req.body;
+    console.log('pets post was hit!');
+    // Add an INSERT query
+    pool.connect(function(errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            //when connecting to database failed
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            // when connecting to database worked aka HAPPYPATH!
+            client.query('INSERT INTO owners (first_name, last_name) VALUES ($1, $2);', [newOwner.firstName, newOwner.lastName], function(errorMakingQuery, result) {
+                done(); //needed
+                if (errorMakingQuery) {
+                    console.log('Error making database query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.sendStatus(201);
+                }
+            });
+        }
+    });
+});
 module.exports = router;
